0b3be8640643b665f9985fc0825a7d2a0c244e95
Wells <wells@polydice.com>
2020-08-13 21:42:01 +0800

    Fix wifiService init

--- app/src/main/java/com/example/network/MainActivity.kt
+++ app/src/main/java/com/example/network/MainActivity.kt
@@ -12,28 +12,36 @@ import kotlinx.android.synthetic.main.content_main.*
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.net.InetAddress
+import java.net.Proxy
+import java.net.Socket
+import javax.net.SocketFactory
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var receiver: NetworkReceiver
-    lateinit var connMgr: ConnectivityManager
+    private lateinit var connMgr: ConnectivityManager
+    private lateinit var wifiManager: WifiManager
     var wifiClient: OkHttpClient? = null
     var systemDefaultClient: OkHttpClient = OkHttpClient.Builder().build()
-    lateinit var wifiService: StatusService
+    var wifiService: StatusService? = null
     lateinit var serviceSystemDefault: StatusService
-    private var isWifiConnected = false
-    private var isNetworkConnected = false
+
+    private var isWifiEnable = false
+
+    //Connect to public network
+    private var isPublicNetworkConnected = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
         logcat.movementMethod = ScrollingMovementMethod()
-        connMgr =
-            application.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-
-        initWifiService()
+        connMgr = application.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        wifiManager = application.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        setWifiStatus(wifiManager.isWifiEnabled)
 
         val retrofitSystemDefault = Retrofit.Builder().client(systemDefaultClient)
             .baseUrl("https://code-test.migoinc-dev.com/")
@@ -44,14 +52,19 @@ class MainActivity : AppCompatActivity() {
 
         btn_call_api.setOnClickListener {
 
-            if (isNetworkConnected) {
-                if (isWifiConnected) {
+            when {
+                isWifiEnable -> {
+                    if (wifiService == null) {
+                        initWifiService()
+                    }
                     callPrivateAPI()
-                } else {
+                }
+                isPublicNetworkConnected -> {
                     callPublicAPI()
                 }
-            } else {
-                logcat.append("There is no networks now, Please check network")
+                else -> {
+                    logcat.append("There is no networks now, Please check network")
+                }
             }
 
         }
@@ -66,8 +79,6 @@ class MainActivity : AppCompatActivity() {
         receiver.listener = object : NetworkReceiver.ConnectionChangeListener {
             override fun onConnectionChange() {
                 logcat.append("--------Connection dedicated!!--------\n")
-                val wifiManager =
-                    application.getSystemService(Context.WIFI_SERVICE) as WifiManager
                 setWifiStatus(wifiManager.isWifiEnabled)
                 setCurrentNetworkInfo(connMgr.allNetworks)
             }
@@ -77,19 +88,19 @@ class MainActivity : AppCompatActivity() {
         registerReceiver(receiver, filter)
     }
 
-    private fun initWifiService(
-    ) {
+    private fun initWifiService() {
         val wifiInfo = connMgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI)
         if (wifiInfo != null && wifiInfo.isConnectedOrConnecting) {
-            val wifiNetwork =
-                connMgr.allNetworks.firstOrNull { connMgr.getNetworkInfo(it)!!.type == ConnectivityManager.TYPE_WIFI }!!
-            wifiClient = OkHttpClient.Builder().socketFactory(wifiNetwork.socketFactory).build()
-            val retrofitWifi = Retrofit.Builder().client(wifiClient!!)
-                .baseUrl("http://192.168.2.2/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
+            val wifiNetwork = connMgr.allNetworks.firstOrNull { connMgr.getNetworkInfo(it)!!.type == ConnectivityManager.TYPE_WIFI }?.let {
+                        wifiClient = OkHttpClient.Builder().proxy(Proxy.NO_PROXY)
+                            .socketFactory(it.socketFactory).build()
+                        val retrofitWifi = Retrofit.Builder().client(wifiClient!!)
+                            .baseUrl("http://192.168.2.2/")
+                            .addConverterFactory(GsonConverterFactory.create())
+                            .build()
 
-            wifiService = retrofitWifi.create(StatusService::class.java)
+                        wifiService = retrofitWifi.create(StatusService::class.java)
+                    }
         }
 
 
@@ -97,7 +108,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun callPrivateAPI() {
         logcat.append("Call private api...\n")
-        wifiService.getStatus().enqueue(object : retrofit2.Callback<Status> {
+        wifiService?.getStatus()?.enqueue(object : retrofit2.Callback<Status> {
             override fun onFailure(call: retrofit2.Call<Status>, t: Throwable) {
                 logcat.append("Call private api fail: ${t.message} \n")
                 callPublicAPI()
@@ -129,7 +140,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     public fun setWifiStatus(enable: Boolean) {
-
+        isWifiEnable = enable
         logcat.append(
             if (enable) {
                 "Wifi Current :Enable\n"
@@ -141,7 +152,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     public fun setCurrentNetworkInfo(networkWorks: Array<Network>) {
-        logcat.append("目前的網路連線：\n")
+        logcat.append("目前的公用網路連線：\n")
         var newConnectionStatus = false
         networkWorks.forEach {
             val networkInfo = connMgr.getNetworkInfo(it)
@@ -149,14 +160,13 @@ class MainActivity : AppCompatActivity() {
                 newConnectionStatus = true
                 if (networkInfo.type == ConnectivityManager.TYPE_WIFI) {
                     logcat.append("Wifi:Connected\n")
-                    isWifiConnected = true
                 } else {
                     logcat.append("other:Connected\n")
                 }
             }
         }
-        isNetworkConnected = newConnectionStatus
-        if (!isNetworkConnected) {
+        isPublicNetworkConnected = newConnectionStatus
+        if (!isPublicNetworkConnected) {
             logcat.append("無\n")
         }
 
